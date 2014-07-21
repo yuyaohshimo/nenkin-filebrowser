3ac30817f86b87b1d58dbdb90ea63158aec78b7e
oshimo_yuya <oshimo_yuya@cyberagent.co.jp>
2014-07-21 15:35:13 +0900

    add server

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,44 @@
-var fs = require('fs');
 var vp = $.viewport();
 var imagePath = './img';
-var images = fs.readdirSync(imagePath);
+var images = [];
+var cacheImageName = [];
+var cacheImg = {};
 var lastImageIndex = 0;
 var MAIN_INTERVAL = 60000; // 60 seconds
 var isResizing = false;
 
-// Function
-var deleteExtraFile = function () {
-	for (var i = 0; i < images.length; i++) {
-		if (images[i].indexOf('png') === -1 && images[i].indexOf('jpg') === -1 && images[i].indexOf('JPG') === -1) {
-			images.splice(i--, 1);
+// ajax
+var getImageNames = function (callback) {
+	$.http.get('http://localhost:3000/api/imageNames', {
+		complete: function (data) {
+			callback(null, JSON.parse(data));
+		},
+		error: function (err) {
+			callback(err);
 		}
-	}
+	});
 };
-deleteExtraFile();
+
+// Functions
+var cache = function () {
+	images.forEach(function (image) {
+		if (cacheImageName.indexOf(image) === -1) {
+			cacheImg[image] = new Image();
+			cacheImg[image].src = imagePath + '/' + image;
+			cacheImageName.push(image);
+		}
+	});
+};
 
 var refresh = function () {
-	images = fs.readdirSync(imagePath);
-	deleteExtraFile();
+	getImageNames(function (err, data) {
+		if (err) {
+			console.log(err);
+		} else {
+			images = data;
+			cache();
+		}
+	});
 };
 
 var getImagePath = function (imageName) {
@@ -101,17 +121,27 @@ $('#knob').on('mousedown', function (e) {
 
 });
 
-// Style
-$('body').css({
-	widht: vp.window.width,
-	height: vp.window.height
-});
+getImageNames(function (err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		images = data;
+		cache();
 
-$('#main_image').css({
-	backgroundImage: getImagePath(images[0])
-});
+		// Style
+		$('body').css({
+			widht: vp.window.width,
+			height: vp.window.height
+		});
 
-// Timer
-var mainTimer = setInterval(function() {
-	refresh();
-}, MAIN_INTERVAL);
+		$('#main_image').css({
+			backgroundImage: getImagePath(images[0])
+		});
+
+		// Timer
+		var mainTimer = setInterval(function() {
+			refresh();
+		}, MAIN_INTERVAL);
+
+	}
+});
---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+app.set('views', __dirname + '/public/jade');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+	res.render('index');
+});
+
+app.get('/api/imageNames', function (req, res) {
+	var imagePath = './public/img';
+	var images = fs.readdirSync(imagePath);
+
+	// delete extra files
+	for (var i = 0; i < images.length; i++) {
+		if (images[i].indexOf('png') === -1 && images[i].indexOf('jpg') === -1 && images[i].indexOf('JPG') === -1) {
+			images.splice(i--, 1);
+		}
+	}
+
+	res.json(images);
+});
+
+app.listen(3000);
