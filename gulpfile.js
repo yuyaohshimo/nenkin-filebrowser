f650be12f2f1040a2d86a96287ae0528173b963f
oshimo_yuya <oshimo_yuya@cyberagent.co.jp>
2014-07-23 14:34:13 +0900

    add image optimize

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
 var imageResize = require('gulp-image-resize');
+var imagemin = require('gulp-imagemin');
 var watch = require('gulp-watch');
 
 gulp.task('default', function () {
 	watch({ glob: 'public/img/*.{jpg,png,JPG}' }, function(files) {
 		return files
 		.pipe(imageResize({ width : 2048 }))
+		.pipe(imagemin({
+			progressive: true
+		}))
 		.pipe(gulp.dest('public/resizedImg/'));
 	});
 });
